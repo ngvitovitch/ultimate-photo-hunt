186e21e8d3d1cd35edcf7b14069b749fcea9ead4
Nicholas Vitovitch <ngvitovitch@gmail.com>
2013-12-08 20:22:52 -0500

    Adds a welcome splash page, a tutorial pop-up, and templates a game main page

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,18 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+$(document).on('pageinit', function(){
+	$('#hunt-button').on('click',function(){
+		$('#hunt-panel').panel('open');
+	});
+	$('#content').on('swiperight',function(){
+		$('#hunt-panel').panel('open');
+	});
+
+	$('#team-button').on('click',function(){
+		$('#team-panel').panel('open');
+	});
+	$('#content').on('swipeleft', function(){
+		$('#team-panel').panel('open');
+	});
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Scavenger::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: 
+  #   first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  # Tutorial
+  get 'tutorial' => 'welcome#tutorial'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
